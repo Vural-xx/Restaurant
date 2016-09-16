53bda12862819a979d7731b44f5a8a0bdc5e5813
Vural Yilmaz <vyilmaz@stud.hs-bremen.de>
2016-09-16 17:35:07 +0200

    Initial commit

---
+++ app/src/main/java/nl/hs_hague/restaurant/model/Restaurant.java
@@ -0,0 +1,70 @@
+package nl.hs_hague.restaurant.model;
+
+/**
+ * Created by vural on 16.09.16.
+ */
+public class Restaurant {
+    private int id;
+    private String name;
+    private String description;
+    private String place;
+    private String street;
+    private String zip;
+    private String image;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
