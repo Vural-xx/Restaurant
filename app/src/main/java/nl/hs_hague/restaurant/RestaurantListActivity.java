3aa8a1bd38a98f333ee66dc82cc722bd578933c4
Vural Yilmaz <vyilmaz@stud.hs-bremen.de>
2016-09-16 19:10:15 +0200

    test

--- app/src/main/java/nl/hs_hague/restaurant/RestaurantListActivity.java
+++ app/src/main/java/nl/hs_hague/restaurant/RestaurantListActivity.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -41,6 +42,7 @@ public class RestaurantListActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
 
+        Log.d("TEst", "test");
 
         View recyclerView = findViewById(R.id.restaurant_list);
         assert recyclerView != null;
